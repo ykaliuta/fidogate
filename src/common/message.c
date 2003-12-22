6b2e07b98a296f9c1184cf9cf05d55140d4571b6
Andrey Slusar <rusfidogate@users.sourceforge.net>
2003-12-22 21:10:07 +0000

    See ChangeLog

--- src/common/message.c
+++ src/common/message.c
@@ -821,8 +821,6 @@ short int pkt_get_body_parse(FILE *fp, MsgBody *body, Node *from, Node *to)
 	    debug(9, "WARNING: no ' * Origin:' line!");
 #ifdef INSERT_ORIGIN
 	    sprintf(buffer, " * Origin: (%s)\r", znfp1(from));
-#else
-	    sprintf(buffer, " * Origin: \r");
 #endif
 	    body->origin = strsave(buffer);
 	}
