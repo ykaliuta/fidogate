3f905db9b51514369ba4afadb6595389079d29e1
Andrey Slusar <anrays@gmail.com>
2004-08-01 06:16:12 +0000

    See ChangeLog

--- src/common/getopt.c
+++ src/common/getopt.c
@@ -52,6 +52,7 @@ char *alloca ();
 #endif
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /* Comment out all this code if we are using the GNU C Library, and are not
    actually compiling the library itself.  This code is part of the GNU C
