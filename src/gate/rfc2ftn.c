a769e3e2f706313fccbaac3d229cc3ceffb71c6d
Andrey Slusar <anrays@gmail.com>
2007-09-12 16:59:58 +0000

    See ChangeLog

--- src/gate/rfc2ftn.c
+++ src/gate/rfc2ftn.c
@@ -1469,7 +1469,7 @@ again:
 	    /* Generate FSC-0035 ^AREPLYADDR, ^AREPLYTO */
 	    pt = xlat_s(s_rfcaddr_to_asc(&rfc_from, FALSE), pt);
 	    if(replyaddr_ifmail_tx)
-		fprintf(sf, "\001REPLYADDR %s <%s>\r\n", msg->name_from,
+		fprintf(sf, "\001REPLYADDR: %s <%s>\r\n", msg->name_from,
 			pt ? pt : s_rfcaddr_to_asc(&rfc_from, FALSE) );
 	    else
 		fprintf(sf, "\001REPLYADDR %s\r\n",
@@ -1481,7 +1481,7 @@ again:
 	    else
 #endif
 		if(replyaddr_ifmail_tx)
-		    fprintf(sf, "\001REPLYTO %s UUCP\r\n", znf1(cf_addr()));
+		    fprintf(sf, "\001REPLYTO: %s UUCP\r\n", znf1(cf_addr()));
 		else
 		    fprintf(sf, "\001REPLYTO %s %s\r\n", znf1(cf_addr()),
 			    msg->name_from);
