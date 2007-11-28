200478a746613db88854645479114030b8773774
Yauheni Kaliuta <y.kaliuta@gmail.com>
2007-11-28 23:52:54 +0200

    Added debug to mime_enheader()

--- src/common/mime.c
+++ src/common/mime.c
@@ -198,6 +198,9 @@ int mime_enheader(char **dst, char *src, size_t len, char *encoding)
     int i;
     int outpos = 0;
     char *delim = NULL;
+
+    debug(6, "MIME: %s: %d chars to encode (%s): %s",
+	  __FUNCTION__, len, encoding);
     
     padding = (B64_ENC_CHUNK - len % B64_ENC_CHUNK) % B64_ENC_CHUNK;
     
@@ -274,6 +277,8 @@ int mime_enheader(char **dst, char *src, size_t len, char *encoding)
 
     if(encoding != NULL)
         strcat(buf, MIME_HEADER_CODE_END);
+
+    debug(6, "MIME: %s: encoded: %s", __FUNCTION__, buf);
 
     xfree(delim);
     return OK;
