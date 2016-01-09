c9b078737db29c22043b8ad6bedaf9ae60c113a0
Yauheni Kaliuta <y.kaliuta@gmail.com>
2016-01-09 23:38:42 +0200

    tests: rfc2ftn: dummy snd_mail call
    Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

--- tests/gate/test-rfc2ftn.c
+++ tests/gate/test-rfc2ftn.c
@@ -3,9 +3,35 @@
  */
 
 #include "rfc2ftn.h"
+#include "prototypes.h"
+#include <cgreen/cgreen.h>
+
+int snd_mail(RFCAddr rfc_to, Textlist *body, long size);
+
+Ensure(dummy)
+{
+    RFCAddr to = { 0 };
+    Textlist body = { 0 };
+    int r;
+    long size = 1000;
+
+    r = snd_mail(to, &body, size);
+    assert_that(r, is_equal_to(0));
+}
+
+static TestSuite *create_rfc2ftn_suite(void)
+{
+    TestSuite *suite = create_named_test_suite(
+	"rfc2ftn suite");
+
+    add_test(suite, dummy);
+
+    return suite;
+}
 
 int main(int argc, char **argv)
 {
-    return 0;
+    TestSuite *suite = create_rfc2ftn_suite();
+    return run_test_suite(suite, create_text_reporter());
 }
 
