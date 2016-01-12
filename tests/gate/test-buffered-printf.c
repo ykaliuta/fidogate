b5814384b4977915ddab785fae000ac7fb3a4f84
Yauheni Kaliuta <y.kaliuta@gmail.com>
2016-01-12 21:07:12 +0200

    test: buffered-printf: check long expanding

--- tests/gate/test-buffered-printf.c
+++ tests/gate/test-buffered-printf.c
@@ -78,27 +78,56 @@ Ensure(bp_understands_format)
 
 static char exp_extends[DEFAULT_SIZE + 1];
 
+static void
+fill_array(char *arr, size_t size, char c)
+{
+    int i;
+
+    for (i = 0; i < size - 1; i++)
+	arr[i] = c;
+    arr[size - 1] = '\0';
+}
+
 static char *
 gen_extends_exp(void)
 {
-    int i;
-    int tail_idx = DEFAULT_SIZE;
-    char c = 'a';
-
-    for (i = 0; i < DEFAULT_SIZE; i++)
-	exp_extends[i] = c;
-    exp_extends[tail_idx] = '\0';
+    fill_array(exp_extends, sizeof(exp_extends), 'a');
     return exp_extends;
+}
+
+static void
+bp_print_n_chars(char c, size_t n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+	bp_printf(buffer_id, "%c", c);
 }
 
 Ensure(bp_extends)
 {
     char *exp = gen_extends_exp();
-    int i;
-    char c = 'a';
 
-    for (i = 0; i < DEFAULT_SIZE; i++)
-	bp_printf(buffer_id, "%c", c);
+    bp_print_n_chars('a', DEFAULT_SIZE);
+
+    s = bp_buffer_content(buffer_id);
+    assert_that(s, is_equal_to_string(exp));
+}
+
+static char long_string[2048];
+
+static char *
+gen_long_string(void)
+{
+    fill_array(long_string, sizeof(long_string), 'a');
+    return long_string;
+}
+
+Ensure(bp_extends_long)
+{
+    char *exp = gen_long_string();
+
+    bp_print_n_chars('a', sizeof(long_string) - 1);
 
     s = bp_buffer_content(buffer_id);
     assert_that(s, is_equal_to_string(exp));
@@ -115,6 +144,7 @@ static TestSuite *create_bp_suite(void)
     add_test(suite, bp_destinguishes_buffer_id);
     add_test(suite, bp_understands_format);
     add_test(suite, bp_extends);
+    add_test(suite, bp_extends_long);
 
     set_setup(suite, bp_test_setup);
     set_teardown(suite, bp_test_teardown);
