7b36691f0e60fa33b1480a443a3b00acdaabbf26
Yauheni Kaliuta <y.kaliuta@gmail.com>
2016-01-12 22:38:12 +0200

    tests: snd_mail: set setup, clean log bufs

--- tests/gate/test-snd_mail.c
+++ tests/gate/test-snd_mail.c
@@ -3,6 +3,7 @@
 
 #include "rfc2ftn.h"
 #include "prototypes.h"
+#include "mock-log.h"
 #include <cgreen/cgreen.h>
 
 int snd_mail(RFCAddr rfc_to, Textlist *body, long size);
@@ -19,12 +20,19 @@ Ensure(dummy)
     assert_that(r, is_equal_to(0));
 }
 
+static void setup(void)
+{
+    log_buffer_release();
+    debug_buffer_release();
+}
 
 TestSuite *create_snd_mail_suite(void)
 {
     TestSuite *suite = create_named_test_suite("snd_mail suite");
 
     add_test(suite, dummy);
+
+    set_setup(suite, setup);
 
     return suite;
 }
