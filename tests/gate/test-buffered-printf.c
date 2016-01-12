493ba9afded9ab524eb8cdd4838166ae5bc983c6
Yauheni Kaliuta <y.kaliuta@gmail.com>
2016-01-12 20:44:23 +0200

    tests: buffered_printf: add bp_extends test

--- tests/gate/test-buffered-printf.c
+++ tests/gate/test-buffered-printf.c
@@ -65,11 +65,40 @@ Ensure(bp_destinguishes_buffer_id)
     assert_that(s, is_equal_to_string(exp2));
 }
 
-Ensure(bp_understand_format)
+Ensure(bp_understands_format)
 {
     char *exp = TEST_STR "10" TEST_STR;
 
     bp_printf(buffer_id, TEST_STR "%d%s", 10, TEST_STR);
+
+    s = bp_buffer_content(buffer_id);
+    assert_that(s, is_equal_to_string(exp));
+}
+#define DEFAULT_SIZE 2048
+
+static char exp_extends[DEFAULT_SIZE + 1];
+
+static char *
+gen_extends_exp(void)
+{
+    int i;
+    int tail_idx = DEFAULT_SIZE;
+    char c = 'a';
+
+    for (i = 0; i < DEFAULT_SIZE; i++)
+	exp_extends[i] = c;
+    exp_extends[tail_idx] = '\0';
+    return exp_extends;
+}
+
+Ensure(bp_extends)
+{
+    char *exp = gen_extends_exp();
+    int i;
+    char c = 'a';
+
+    for (i = 0; i < DEFAULT_SIZE; i++)
+	bp_printf(buffer_id, "%c", c);
 
     s = bp_buffer_content(buffer_id);
     assert_that(s, is_equal_to_string(exp));
@@ -84,7 +113,8 @@ static TestSuite *create_bp_suite(void)
     add_test(suite, bp_outputs_string_to_buffer);
     add_test(suite, bp_adds_string_to_buffer);
     add_test(suite, bp_destinguishes_buffer_id);
-    add_test(suite, bp_understand_format);
+    add_test(suite, bp_understands_format);
+    add_test(suite, bp_extends);
 
     set_setup(suite, bp_test_setup);
     set_teardown(suite, bp_test_teardown);
