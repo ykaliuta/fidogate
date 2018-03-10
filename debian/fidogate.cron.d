aa87f66a28d3762d01cc41d8fe9bc23b6ab72b87
Yauheni Kaliuta <y.kaliuta@gmail.com>
2018-03-10 07:51:22 +0200

    packaging: cron.d run runinc sequentially
    Run runinc and runinc -o use the same lock (the same runinc
    program) so if they are started at the same time by cron,
    conflict happens:
    
    runinc ERROR: cannot read lock file (errno=0: Success)
    
    Start output processing (runinc -o) after input
    processing (runinc) is finished.
    
    Patch from: Alex Shupikov.
    
    [ YK: && replaced by ;, we do not care if the input processing
    succeeded ]
    
    Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

--- debian/fidogate.cron.d
+++ debian/fidogate.cron.d
@@ -1,7 +1,5 @@
 #
 # Regular cron jobs for the fidogate package
-#
-*/10 *	* * *	ftn	/usr/bin/runinc
-*/10 *	* * *	news	/usr/bin/send-fidogate
-*/10 *	* * *	ftn	/usr/bin/runinc -o
 
+*/10 * * * *	news	/usr/bin/send-fidogate
+*/10 * * * *	ftn	/usr/bin/runinc ; /usr/bin/runinc -o
