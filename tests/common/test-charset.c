00863e6d497683a562bfe98c0c1c996a4622a50f
Yauheni Kaliuta <y.kaliuta@gmail.com>
2019-11-13 01:36:40 +0200

    tests: add test_charset.
    One test now, check if LATIN-1 supported.
    
    Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

---
+++ tests/common/test-charset.c
@@ -0,0 +1,43 @@
+#include <cgreen/cgreen.h>
+#include <stdio.h>
+
+#include "../../src/include/prototypes.h"
+
+void debug(int lvl, const char *fmt, ...)
+{
+}
+
+void fglog(const char *fmt, ...)
+{
+}
+
+Ensure(charset_recodes_latin1)
+{
+	char *src = "\xf8";
+	char *exp = "ø";
+	char res[16];
+	size_t dstlen = sizeof(res);
+	size_t srclen = strlen(src) + 1;
+
+	charset_recode_string(res, &dstlen, src, &srclen, "LATIN-1", "UTF-8");
+
+	assert_that(res, is_equal_to_string(exp));
+}
+
+static TestSuite *create_mime_suite(void)
+{
+    TestSuite *suite = create_named_test_suite(
+	    "charset suite");
+    add_test(suite, charset_recodes_latin1);
+    return suite;
+}
+
+int main(int argc, char **argv)
+{
+    TestSuite *suite = create_mime_suite();
+
+    if (argc > 1)
+	return run_single_test(suite, argv[1], create_text_reporter());
+
+    return run_test_suite(suite, create_text_reporter());
+}
