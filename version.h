d7ef0c68c1b723ecc4710f54a5fa8b1bbb116797
Yauheni Kaliuta <y.kaliuta@gmail.com>
2020-01-05 17:57:13 +0200

    Fidogate 5.6
    Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

--- version.h
+++ version.h
@@ -2,8 +2,6 @@
 /*****************************************************************************
  * FIDOGATE --- Gateway software UNIX Mail/News <-> FIDO NetMail/EchoMail
  *
- * $Id: version.h,v 5.4 2006/05/18 18:35:08 anray Exp $
- *
  * Global version number, patch level
  *****************************************************************************/
 
@@ -12,7 +10,7 @@
 
 #include <config.h>
 #define VERSION_MAJOR	5
-#define VERSION_MINOR	5
+#define VERSION_MINOR	6
 
 #ifdef GIT_HASH_STR
 #define EXTRAVERSION	"-g" GIT_HASH_STR
