946298da3604e4fe0940df287f4126f3b4eb8e39
Yauheni Kaliuta <y.kaliuta@gmail.com>
2020-03-12 20:02:38 +0200

    tests/date: date_tz: check positive and negative TZ conversion
    Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

--- tests/common/test-date.c
+++ tests/common/test-date.c
@@ -36,6 +36,30 @@ Ensure(parsedate_handles_google)
     assert_that(res, is_equal_to(exp));
 }
 
+Ensure(date_tz_converts_positive_tz)
+{
+    time_t src = 1583930729;
+    char *exp = "11 Mar 20  14:45:29";
+    char *tz = "0200";
+    char *res;
+
+    res = date_tz(DATE_FTS_0001, &src, tz);
+
+    assert_that(res, is_equal_to_string(exp));
+}
+
+Ensure(date_tz_converts_negative_tz)
+{
+    time_t src = 1583930729;
+    char *exp = "11 Mar 20  05:45:29";
+    char *tz = "-0700";
+    char *res;
+
+    res = date_tz(DATE_FTS_0001, &src, tz);
+
+    assert_that(res, is_equal_to_string(exp));
+}
+
 static TestSuite *create_suite(void)
 {
     TestSuite *suite = create_named_test_suite("Date suite");
@@ -43,6 +67,8 @@ static TestSuite *create_suite(void)
     add_test(suite, date_handles_via_format);
     add_test(suite, parsedate_handles_rfc822);
     add_test(suite, parsedate_handles_google);
+    add_test(suite, date_tz_converts_positive_tz);
+    add_test(suite, date_tz_converts_negative_tz);
 
     return suite;
 }
