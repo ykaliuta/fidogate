8d0451e5ba799a71b9fef2be169484ca458a5eb7
Yauheni Kaliuta <y.kaliuta@gmail.com>
2020-03-12 19:17:28 +0200

    tests/date: parsedate: check parsing of rfc822 and google dates
    Google adds timezone's name.
    
    Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

--- tests/common/test-date.c
+++ tests/common/test-date.c
@@ -12,11 +12,37 @@ Ensure(date_handles_via_format)
     assert_that(res, is_not_null);
 }
 
+Ensure(parsedate_handles_rfc822)
+{
+    char *src = "Wed, 11 Mar 2020 05:45:29 -0700";
+    time_t exp = 1583930729;
+    time_t res;
+    TIMEINFO *default_now = NULL;
+
+    res = parsedate(src, default_now);
+
+    assert_that(res, is_equal_to(exp));
+}
+
+Ensure(parsedate_handles_google)
+{
+    char *src = "Wed, 11 Mar 2020 05:45:29 -0700 (PDT)";
+    time_t exp = 1583930729;
+    time_t res;
+    TIMEINFO *default_now = NULL;
+
+    res = parsedate(src, default_now);
+
+    assert_that(res, is_equal_to(exp));
+}
+
 static TestSuite *create_suite(void)
 {
     TestSuite *suite = create_named_test_suite("Date suite");
 
     add_test(suite, date_handles_via_format);
+    add_test(suite, parsedate_handles_rfc822);
+    add_test(suite, parsedate_handles_google);
 
     return suite;
 }
