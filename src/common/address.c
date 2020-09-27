ykaliuta/fidogate

commit ba71ee86c9abcba6024fa61a3ed9aaa06fddb11e

rfc2ftn: skip names/subject translating on crossposting
If rfc message is posted to several newsgroups the messages are
created for all the echoareas separately. But actually it is not
started from scratch since the header's fields (name_to, name_from
and subject) are handled "in-place" (well, copied back after
handling) so come already translated (recoded) after the first
message is done.

Add a flag and skip the translation phase after it's done once. It
requires to initialize the 'pt' pointer for proper memory
management.

Other option would be to reinitialize the fields for every
newsgroup.

Reported-by: Eugene Subbotin 2:5075/35@fidonet
Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

src/gate/rfc2ftn.c
@@ -722,7 +722,7 @@ int snd_mail(RFCAddr rfc_to, long size)
     char *p;
     char subj[MSG_MAXSUBJ * 4 + 1];
     int status, fido;
-    Message msg;
+    Message msg = { 0 };
     char *flags = NULL;
     MIMEInfo *mime;
     int from_is_local = FALSE;
@@ -1277,55 +1277,60 @@ int snd_message(Message * msg, Area * parea,
 
  again:
 
-    pt = xlat_s(subj, NULL);
-
-    /* Subject with split part indication */
-    if (split && part > 1) {
-        str_printf(msg->subject, sizeof(msg->subject), "%02d: ", part);
-        BUF_APPEND(msg->subject, pt ? pt : subj);
+    if (msg->translated) {
+	pt = NULL;
     } else {
-        BUF_COPY(msg->subject, pt ? pt : subj);
-#ifdef MAXMSGHEADRLEN
-        if (strlen(msg->subject) > MAXMSGHEADRLEN) {
-            BUF_COPY(tmp_msg, buffer);
-            sprintf(buffer, "Subject: %s\n", pt);
-            BUF_APPEND(buffer, tmp_msg);
-            size++;
-            BUF_COPY(msg->subject, "(no subject)");
-        }
-#endif                          /* MAXMSGHEADRLEN */
-    }
+	msg->translated = true;
 
-    pt = xlat_s(msg->name_to, pt);
-    if (pt) {
-#ifdef MAXMSGHEADRLEN
-        if (strlen(msg->name_to) > MAXMSGHEADRLEN) {
-            BUF_COPY(tmp_msg, buffer);
-            sprintf(buffer, "To: %s\n", msg->name_to);
-            BUF_APPEND(buffer, tmp_msg);
-            size++;
-            if (!parea && msg->node_to.node == 128)
-                BUF_COPY(msg->subject, "UUCP");
-            else
-                BUF_COPY(msg->subject, "SysOp");
-        } else
-#endif                          /* MAXMSGHEADRLEN */
-            BUF_COPY(msg->name_to, pt);
-    }
+	pt = xlat_s(subj, NULL);
 
-    pt = xlat_s(msg->name_from, pt);
-    if (pt) {
+	/* Subject with split part indication */
+	if (split && part > 1) {
+	    str_printf(msg->subject, sizeof(msg->subject), "%02d: ", part);
+	    BUF_APPEND(msg->subject, pt ? pt : subj);
+	} else {
+	    BUF_COPY(msg->subject, pt ? pt : subj);
 #ifdef MAXMSGHEADRLEN
-        if (strlen(msg->name_from) > MAXMSGHEADRLEN) {
-            BUF_COPY(tmp_msg, buffer);
-            sprintf(buffer, "From: %s\n", msg->name_from);
-            BUF_APPEND(buffer, tmp_msg);
-            size++;
-        } else
+	    if (strlen(msg->subject) > MAXMSGHEADRLEN) {
+		BUF_COPY(tmp_msg, buffer);
+		sprintf(buffer, "Subject: %s\n", pt);
+		BUF_APPEND(buffer, tmp_msg);
+		size++;
+		BUF_COPY(msg->subject, "(no subject)");
+	    }
 #endif                          /* MAXMSGHEADRLEN */
-            BUF_COPY(msg->name_from, pt);
-    }
+	}
 
+	pt = xlat_s(msg->name_to, pt);
+	if (pt) {
+#ifdef MAXMSGHEADRLEN
+	    if (strlen(msg->name_to) > MAXMSGHEADRLEN) {
+		BUF_COPY(tmp_msg, buffer);
+		sprintf(buffer, "To: %s\n", msg->name_to);
+		BUF_APPEND(buffer, tmp_msg);
+		size++;
+		if (!parea && msg->node_to.node == 128)
+		    BUF_COPY(msg->subject, "UUCP");
+		else
+		    BUF_COPY(msg->subject, "SysOp");
+	    } else
+#endif                          /* MAXMSGHEADRLEN */
+		BUF_COPY(msg->name_to, pt);
+	}
+
+	pt = xlat_s(msg->name_from, pt);
+	if (pt) {
+#ifdef MAXMSGHEADRLEN
+	    if (strlen(msg->name_from) > MAXMSGHEADRLEN) {
+		BUF_COPY(tmp_msg, buffer);
+		sprintf(buffer, "From: %s\n", msg->name_from);
+		BUF_APPEND(buffer, tmp_msg);
+		size++;
+	    } else
+#endif                          /* MAXMSGHEADRLEN */
+		BUF_COPY(msg->name_from, pt);
+	}
+    }
     /* Header */
     nmsg++;
     pkt_put_msg_hdr(sf, msg, TRUE);

src/include/packet.h
@@ -99,7 +99,7 @@ typedef struct {
     char name_to[MSG_MAXNAME];  /* To name */
     char name_from[MSG_MAXNAME];    /* From name */
     char subject[MSG_MAXSUBJ];  /* Subject */
-
+    int translated;             /* name_* and subject already xlat'ed */
     char *area;                 /* EchoMail area or NULL */
 } Message;
 

commit 6f5a4dfd21fbbe21249bb014a71011b353c32e6c

rfc2ftn: factor out snd_mail_prepare() and call for every group
Move common news/mail starting code to own function.

This is a preparation to handle cross posting to groups with
different settings (charset) in 'areas'

This patch changes the logic a bit, for crossposting it calls the
preparation code not once in the beginning of rfc message
processing, but for every newsgroup. To make it working the

ba71ee86c9ab ("rfc2ftn: skip names/subject translating on crossposting")

is reverted, the names/string are generated from the header before
evern snd_message() call.

Meanwhile it fixes a bug, introduced by ba71ee86c9ab, where on
splitting subjects did not have part indication.

Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

src/common/address.c
@@ -256,7 +256,7 @@ int addr_is_local_xpost(char *addr)
 
     RFCAddr rfc;
     char ailx;
-    static char **addr_list = NULL;
+    char **addr_list = NULL;
 
     if (!addr)
         return FALSE;

src/gate/rfc2ftn.c
@@ -967,31 +967,26 @@ static int snd_message(Message * msg, Area * parea,
 
  again:
 
-    if (msg->translated) {
-	pt = NULL;
-    } else {
-	msg->translated = true;
+    pt = xlat_s(subj, NULL);
 
-	pt = xlat_s(subj, NULL);
-
-	/* Subject with split part indication */
-	if (split && part > 1) {
+    /* Subject with split part indication */
+    if (split && part > 1) {
 	    str_printf(msg->subject, sizeof(msg->subject), "%02d: ", part);
 	    BUF_APPEND(msg->subject, pt ? pt : subj);
-	} else {
+    } else {
 	    BUF_COPY(msg->subject, pt ? pt : subj);
-	}
-
-	pt = xlat_s(msg->name_to, pt);
-	if (pt) {
-	    BUF_COPY(msg->name_to, pt);
-	}
-
-	pt = xlat_s(msg->name_from, pt);
-	if (pt) {
-	    BUF_COPY(msg->name_from, pt);
-	}
     }
+
+    pt = xlat_s(msg->name_to, pt);
+    if (pt) {
+	    BUF_COPY(msg->name_to, pt);
+    }
+
+    pt = xlat_s(msg->name_from, pt);
+    if (pt) {
+	    BUF_COPY(msg->name_from, pt);
+    }
+
     /* Header */
     nmsg++;
     pkt_put_msg_hdr(sf, msg, TRUE);
@@ -1298,6 +1293,56 @@ static int snd_message(Message * msg, Area * parea,
     return EX_OK;
 }
 
+/* to be called after header decoded */
+static int snd_mail_prepare(RFCHeader *h,
+                            char *subj, size_t subj_size,
+                            RFCAddr *rfc_to, RFCAddr *rfc_from,
+                            Node *node_to, Node *node_from,
+                            Message *msg,
+                            int *is_fido, int *alias)
+{
+    int rc;
+    char *p;
+
+    /*
+     * Subject
+     */
+    if ((p = header_get(h, "Subject")))
+        str_copy(subj, subj_size, p);
+    else
+        str_copy(subj, subj_size, "(no subject)");
+
+    /*
+     * From RFCAddr
+     */
+    *rfc_from = rfc_sender(h);
+
+    /*
+     * To name/node
+     */
+    rc = mail_receiver(rfc_to, node_to, msg->name_to, sizeof(msg->name_to), h);
+    if (rc == ERROR) {
+        if (*address_error)
+            sendback("Address %s:\n  %s",
+                     s_rfcaddr_to_asc(rfc_to, TRUE), address_error);
+        else
+            sendback("Address %s:\n  address/host is unknown",
+                     s_rfcaddr_to_asc(rfc_to, TRUE));
+        return EX_NOHOST;
+    }
+    *is_fido = rfc_isfido();
+
+    cf_set_best(node_to->zone, node_to->net, node_to->node);
+
+    /*
+     * From name/node
+     */
+    *alias = mail_sender(rfc_from, node_from,
+                         msg->name_from, sizeof(msg->name_from), h);
+
+    return 0;
+}
+
 /*
  * Process mail/news message
  */
@@ -1327,48 +1372,12 @@ static int snd_mail(RFCAddr rfc_to, long size, RFCHeader *h)
         debug(3, "RFC To:       %s", s_rfcaddr_to_asc(&rfc_to, TRUE));
 
     /*
-     * Subject
-     */
-    if ((p = header_get(h, "Subject")))
-        BUF_COPY(subj, p);
-    else
-        BUF_COPY(subj, "(no subject)");
-
-    /*
      * MIME header
      */
     mime = get_mime(s_header_getcomplete(h, "MIME-Version"),
                     s_header_getcomplete(h, "Content-Type"),
                     s_header_getcomplete(h, "Content-Transfer-Encoding"));
 
-    /*
-     * From RFCAddr
-     */
-    rfcaddr_init(&rfc_from);
-    rfc_from = rfc_sender(h);
-
-    /*
-     * To name/node
-     */
-    rc = mail_receiver(&rfc_to, &node_to, msg.name_to, sizeof(msg.name_to), h);
-    if (rc == ERROR) {
-        if (*address_error)
-            sendback("Address %s:\n  %s",
-                     s_rfcaddr_to_asc(&rfc_to, TRUE), address_error);
-        else
-            sendback("Address %s:\n  address/host is unknown",
-                     s_rfcaddr_to_asc(&rfc_to, TRUE));
-        TMPS_RETURN(EX_NOHOST);
-    }
-    fido = rfc_isfido();
-
-    cf_set_best(node_to.zone, node_to.net, node_to.node);
-
-    /*
-     * From name/node
-     */
-    alias_found = mail_sender(&rfc_from, &node_from,
-                              msg.name_from, sizeof(msg.name_from), h);
     /*
      * Date
      */
@@ -1387,8 +1396,6 @@ static int snd_mail(RFCAddr rfc_to, long size, RFCHeader *h)
     if (newsmode) {
         Area *pa;
 
-        acl_ngrp(rfc_from, TYPE_ECHOMAIL);
-        from_is_local = addr_is_local_xpost(s_rfcaddr_to_asc(&rfc_from, FALSE));
         /*
          * Check for news control message
          */
@@ -1422,6 +1429,18 @@ static int snd_mail(RFCAddr rfc_to, long size, RFCHeader *h)
                 debug(5, "No FTN area");
             else {
                 AreasBBS *ab;
+
+                rc = snd_mail_prepare(h, subj, sizeof(subj),
+                                      &rfc_to, &rfc_from,
+                                      &node_to, &node_from,
+                                      &msg, &fido, &alias_found);
+                if (rc != 0)
+                    TMPS_RETURN(rc);
+
+                acl_ngrp(rfc_from, TYPE_ECHOMAIL);
+
+                from_is_local = addr_is_local_xpost(
+                    s_rfcaddr_to_asc(&rfc_from, FALSE));
 
                 ab = areasbbs_lookup(pa->area);
                 if (ab) {
@@ -1513,6 +1532,13 @@ static int snd_mail(RFCAddr rfc_to, long size, RFCHeader *h)
         /*
          * NetMail message
          */
+        rc = snd_mail_prepare(h, subj, sizeof(subj),
+                              &rfc_to, &rfc_from,
+                              &node_to, &node_from,
+                              &msg, &fido, &alias_found);
+        if (rc != 0)
+            TMPS_RETURN(rc);
+
         /* Check message size limit */
         limitsize = areas_get_limitmsgsize();
         if (limitsize > 0 && size > limitsize) {

src/include/packet.h
@@ -104,7 +104,6 @@ typedef struct {
     char name_to[NAMEBUFSIZE];  /* To name */
     char name_from[NAMEBUFSIZE];    /* From name */
     char subject[SUBJBUFSIZE];  /* Subject */
-    int translated;             /* name_* and subject already xlat'ed */
     char *area;                 /* EchoMail area or NULL */
 } Message;
 