ykaliuta/fidogate

commit d3cb1fd285f9a2f45d7f7ff4208bb1511f9a3618

mime: fix memleak on multipart decoding
- fix memleak when temporary section body was not freed. Use new
  tl_addtl_move() to move it instead of copying;
- free temporary string from mime_attr_value() in
  get_mime_disposition().

Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

src/common/mime.c
@@ -1406,6 +1406,7 @@ static MIMEInfo *get_mime_disposition(char *ver, char *type, char *enc,
         if (tmp_line != NULL)
             tmp_str = mime_attr_value(tmp_line->line);
         mime->disposition_filename = s_copy(tmp_str);
+        xfree(tmp_str);
         tl_clear(&header_line);
     }
 
@@ -1652,7 +1653,7 @@ static Textlist *mime_debody_multipart(Textlist * body, MIMEInfo * mime,
 
             ptr_body = mime_debody_section(&tmp_body, header, to, ch_fallback);
             if (ptr_body != NULL) {
-                tl_addtl(dec_body, ptr_body);
+                tl_addtl_move(dec_body, ptr_body);
                 if (ptr_body != &tmp_body)
                     xfree(ptr_body);
             }

commit a5b78f2dbdad0243bf4fae91f3ad0a21c90fa064

mime: get_mime_disposition: free allocated string but not tmps
TMPS is a temporary string. The module keeps allocated strings in a
list and at the end frees them all. Sort of garbage collection.

There is a mix of using TMPS and normally allocated string in the
if (disp != NULL) branch. mime_attr_value() makes copy with
strsave() inside. Make it the same way as in the previous branch.

Fixes: d3cb1fd285f9 ("mime: fix memleak on multipart decoding")
Reported-by: Eugene Subbotin 2:5075/35@fidonet
Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

src/common/mime.c
@@ -1425,12 +1425,16 @@ static MIMEInfo *get_mime_disposition(char *ver, char *type, char *enc,
 
     if (disp != NULL) {
         tmp_str = s_copy(disp);
+        /* tl_append() inside makes copies */
         mime_parse_header(&header_line, tmp_str);
+
         tmp_line = tl_get(&header_line, "filename", strlen("filename"));
-        if (tmp_line != NULL)
+        if (tmp_line != NULL) {
             tmp_str = mime_attr_value(tmp_line->line);
-        mime->disposition_filename = s_copy(tmp_str);
-        xfree(tmp_str);
+            mime->disposition_filename = s_copy(tmp_str);
+            xfree(tmp_str);
+        }
+
         tl_clear(&header_line);
     }
 