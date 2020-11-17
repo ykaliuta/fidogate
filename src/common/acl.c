ykaliuta/fidogate

commit 460dd9bc95077a78598bcf5860b7d33081238c23

rfc2ftn: argify: free arrays
list_init() calls argify() which allocates space for the array it
returns (which is called here list).

It must be freed after usage.

There are several places where it is called. In rfc2ftn these are
addr_is_local_xpost() and acl_ngrp(). The latter keeps one as a
static state, so a function to free explicitly added.

Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

src/common/acl.c
@@ -143,13 +143,22 @@ void acl_ngrp(RFCAddr rfc_from, int type)
     return;
 }
 
+void acl_ngrp_free(void)
+{
+    list_free(ngrp_pat_list);
+}
+
 int acl_ngrp_lookup(char *list)
 {
 
-    static char **ngrp_list = NULL;
+    char **ngrp_list = NULL;
+    int ret;
 
     list_init(&ngrp_list, list);
-    return list_match(ngrp_pat_list, ngrp_list);
+    ret = list_match(ngrp_pat_list, ngrp_list);
+    list_free(ngrp_list);
+
+    return ret;
 }
 
 void pna_notify_init(char *list)

src/common/address.c
@@ -269,6 +269,7 @@ int addr_is_local_xpost(char *addr)
     if (!ailx) {
         list_init(&addr_list, addr);
         ailx = list_match(addr_is_local_pats, addr_list);
+        list_free(addr_list);
     }
     return ailx;
 }

src/gate/rfc2ftn.c
@@ -993,6 +993,7 @@ int snd_mail(RFCAddr rfc_to, long size)
         }
 
         tl_clear(&tl);
+        acl_ngrp_free();
     } else {
         /*
          * NetMail message
@@ -1011,6 +1012,7 @@ int snd_mail(RFCAddr rfc_to, long size)
             msg.node_to = node_to;
             status = snd_message(&msg, NULL, rfc_from, rfc_to,
                                  subj, size, flags, fido, mime, &node_from);
+            acl_ngrp_free();
             TMPS_RETURN(status);
         } else {
             if (pna_notify(s_rfcaddr_to_asc(&rfc_from, FALSE))) {
@@ -1024,6 +1026,7 @@ int snd_mail(RFCAddr rfc_to, long size)
                      s_rfcaddr_to_asc(&rfc_from, FALSE), asc_node_to);
             }
 
+            acl_ngrp_free();
             TMPS_RETURN(EX_OK);
         }
     }

src/include/prototypes.h
@@ -52,6 +52,7 @@ int pna_notify(char *);
 int ftnacl_lookup(Node *, Node *, char *);
 void acl_ftn_free(void);
 void acl_free(void);
+void acl_ngrp_free(void);
 
 /* acl_ftn.c */
 #ifdef FTN_ACL

commit 8ac39d446d783944f83c87082899a10a92e01d49

acl: list_free: NULLify ngrp_pat_list
Fix SIGSEGV on list_free. If the list was initialized before and
area is not found, list_free is called and tries to free list which
was not allocated by list_init.

Fixes: 460dd9bc9507 ("rfc2ftn: argify: free arrays")
Reported-by: Eugene Subbotin 2:5075/35@fidonet
Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

src/common/acl.c
@@ -146,6 +146,8 @@ void acl_ngrp(RFCAddr rfc_from, int type)
 void acl_ngrp_free(void)
 {
     list_free(ngrp_pat_list);
+    /* will be reused with the next acl_ngrp calls */
+    ngrp_pat_list = NULL;
 }
 
 int acl_ngrp_lookup(char *list)