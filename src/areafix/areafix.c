ykaliuta/fidogate

commit 1253fb76866d21bfe41ea89a8907006b25597799

ftnhatch.c, ftntick.c: enable separate tic password
Some uplinks are using separate passwords for FileFix and tic.
This commit adds the --enable-tick-password option to configure
and defines TIC_PASSWORD to change the password context in passwd
file. When this option is enabled, tic password should be added
as separate string to the passwd file in the following way:

tic    2:5075/35    password

[Yauheni Kaliuta: updated AC_REVISION]

Signed-off-by: Eugene Subbotin <evs@subbotin.org>
Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

src/tick/ftntick.c
@@ -37,7 +37,11 @@
 #define CONFIG		DEFAULT_CONFIG_MAIN
 
 #define MY_AREASBBS	"FAreasBBS"
+#ifdef TIC_PASSWORD
+#define MY_CONTEXT	"tic"
+#else
 #define MY_CONTEXT	"ff"
+#endif                          /* TIC_PASSWORD */
 
 #define MY_FILESBBS	"files.bbs"
 

configs/passwd.sample
@@ -40,7 +40,8 @@ packet	2:5030/1229.777		XXXXXXXX
 #
 # Context "af" - areafix passwords for ftnaf
 #         "ff" - filefix passwords for ftnaf -F,
-#                tick passwords for ftnhatch, ftntick.
+#                tick passwords for ftnhatch, ftntick (--disable-tick-password).
+#        "tic" - tick passwords for ftnhatch, ftntick (--enable-tick-password).
 #
 #					lvl key   name
 #					--- ---   ----

configure.ac
@@ -2,7 +2,7 @@ dnl Copyright (c) 2003-2008 Andrey Slusar <anrays@gmail.com>
 dnl Copyright (c) 2003-2004 Alexandr Dobroslavskiy <dsas@users.sourceforge.net>
 dnl Copyright (c) 2015-2020 Yauheni Kaliuta <y.kaliuta@gmail.com>
 dnl
-AC_REVISION([Revision: 1034])
+AC_REVISION([Revision: 1035])
 AC_PREREQ(2.53)
 AC_INIT([fidogate],[5.8],[y.kaliuta@gmail.com])
 AC_DISABLE_OPTION_CHECKING
@@ -576,6 +576,36 @@ if test $enable_tick_crc = yes; then
 	      [Enable if no ftntick CRC control])
 fi
 
+AC_ARG_ENABLE(tick-history,
+[  --disable-tick-history  tick dupe check],dnl
+   [case "${enableval}" in
+    yes) enable_tick_history=yes ;;
+     no) enable_tick_history=no ;;
+      *) AC_MSG_ERROR(bad value ${enableval} for --disable-tick-history) ;;
+    esac],dnl
+    enable_tick_history=yes)
+
+if test $enable_tick_history = yes; then
+    AC_DEFINE(TIC_HISTORY,
+              1,
+	      [Enable tic dupe check])
+fi
+
+AC_ARG_ENABLE(tick-password,
+[  --enable-tick-password  enable separate tic password in passwd file],dnl
+   [case "${enableval}" in
+    yes) enable_tick_password=yes ;;
+     no) enable_tick_password=no ;;
+      *) AC_MSG_ERROR(bad value ${enableval} for --enable-tick-password) ;;
+    esac],dnl
+    enable_tick_password=no)
+
+if test $enable_tick_password = yes; then
+    AC_DEFINE(TIC_PASSWORD,
+              1,
+	      [Enable separate tic password in passwd file])
+fi
+
 AC_ARG_ENABLE(no-organization,
 [  --enable-no-organization enable if no insert Origin line if it absence],dnl
    [case "${enableval}" in
@@ -689,21 +719,6 @@ if test $enable_pid2rd_tid2gtv = yes; then
               1,
 	      [Enable paste ^PID (mail or news reader name)
 	      and ^TID (fidogate version) kludge])
-fi
-
-AC_ARG_ENABLE(tick-history,
-[  --disable-tick-history  tick dupe check],dnl
-   [case "${enableval}" in
-    yes) enable_tick_history=yes ;;
-     no) enable_tick_history=no ;;
-      *) AC_MSG_ERROR(bad value ${enableval} for --disable-tick-history) ;;
-    esac],dnl
-    enable_tick_history=yes)
-
-if test $enable_tick_history = yes; then
-    AC_DEFINE(TIC_HISTORY,
-              1,
-	      [Enable tic dupe check])
 fi
 
 AC_ARG_ENABLE(dnt-netmail,

src/tick/ftnhatch.c
@@ -38,7 +38,11 @@
 #define CREATOR		"by FIDOGATE/ftnhatch"
 
 #define MY_AREASBBS	"FAreasBBS"
+#ifdef TIC_PASSWORD
+#define MY_CONTEXT	"tic"
+#else
 #define MY_CONTEXT	"ff"
+#endif                          /* TIC_PASSWORD */
 
 /*
  * Prototypes

commit 63e6827301418c87a8082b91bda6d52aa9aedbfa

areafix: arearix_init() accept mode as string: af, ff or tic
If compiled with --enable-tick-password there is a problem with
autocreate since areafix_init is inited with "ff" context and
password check fails since current context is "tic" one.

Make it possible to provide tic context to areafix_init().

It requires to add keys like for ff to passwd to make autocreate
working ($ key).

Get rid of MY_CONTEXT define, it duplicates global variable.

Use already existed global variable 'areafix' for mode, not
my_context string. It compared with 'ff' and now it should work for
both 'ff' and 'tic'. It may make sense to revert the condition and
use 'af' to compare and get rid of extra variable ('areafix').

Fixes: 1253fb76866d ("ftnhatch.c, ftntick.c: enable separate tic password")
Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

src/areafix/areafix.c
@@ -41,14 +41,12 @@
 #endif                          /* FTN_ACL */
 
 #define MY_NAME_FF	"Filefix Daemon"
-#define MY_CONTEXT_FF	"ff"
 #define MY_AREASBBS_FF	"FAreasBBS"
 #ifdef FTN_ACL
 #define MY_TYPE_FF	TYPE_FECHO
 #endif                          /* FTN_ACL */
 
 #define MY_NAME		my_name
-#define MY_CONTEXT	my_context
 #ifdef FTN_ACL
 #define MY_TYPE	my_type
 #endif                          /* FTN_ACL */
@@ -91,7 +89,7 @@ static int areafix = TRUE;
 
 /* Program name, context, config areas.bbs name */
 static char *my_name = MY_NAME_AF;
-static char *my_context = MY_CONTEXT_AF;
+static const char *my_context = MY_CONTEXT_AF;
 static char *my_areasbbs = MY_AREASBBS_AF;
 #ifdef FTN_ACL
 static char my_type = MY_TYPE_AF;
@@ -178,27 +176,30 @@ void areafix_stdprintf(const char *fmt, ...)
 /*
  * Common Areafix init
  */
-void areafix_init(int mode)
+void areafix_init(const char *context)
 {
-    areafix = mode;
-
-    if (mode) {
+    if (streq(context, "af")) {
         /* Areafix */
+        areafix = TRUE;
         my_name = MY_NAME_AF;
-        my_context = MY_CONTEXT_AF;
         my_areasbbs = MY_AREASBBS_AF;
 #ifdef FTN_ACL
         my_type = TYPE_ECHO;
 #endif                          /* FTN_ACL */
-    } else {
+    } else if (streq(context, "ff") ||
+	       streq(context, "tic")) {
         /* Filefix */
+        areafix = FALSE;
         my_name = MY_NAME_FF;
-        my_context = MY_CONTEXT_FF;
         my_areasbbs = MY_AREASBBS_FF;
 #ifdef FTN_ACL
         my_type = TYPE_FECHO;
 #endif                          /* FTN_ACL */
+    } else {
+        fprintf(stderr, "Wrong mode: %s, expected af, ff, tic\n", context);
+        exit(EX_USAGE);
     }
+    my_context = context;
 
     /* Get name of areas.bbs file from config file */
     if (!areas_bbs) {
@@ -299,7 +300,7 @@ int areafix_auth_check(Node * node, char *passwd, int checkpass)
 
     /* Check password */
     debug(3, "Node %s, passwd %s", znfp1(node), passwd);
-    pwd = passwd_lookup(MY_CONTEXT, node);
+    pwd = passwd_lookup(my_context, node);
     debug(3, "passwd entry: %s", pwd ? pwd->passwd : "-NONE-");
 
     if (checkpass == FALSE) {
@@ -842,7 +843,7 @@ int cmd_new(Node * node, char *line, char *dwnl, int inter)
     if (p->state == NULL)
         p->state = strsave("S");
     /* if mode filefix */
-    if (!strcmp(my_context, "ff")) {
+    if (!areafix) {
         if (p->flags & AREASBBS_PASSTHRU)   /* -# */
             p->dir = strsave("-");
         else {
@@ -946,7 +947,7 @@ int cmd_new(Node * node, char *line, char *dwnl, int inter)
     } else
         areasbbs_changed();
 
-    if (!strcmp(my_context, "ff")) {
+    if (!areafix) {
 
         for (s1 = cf_get_string("AutoCreateSubscribeFileechoNodes", TRUE);
              s1 && *s1;

src/areafix/ftnaf.c
@@ -46,7 +46,6 @@ extern char *areas_bbs;
 /*
  * Prototypes
  */
-void areafix_init(int);
 int areafix_auth_check(Node *, char *, int);
 void areafix_auth_cmd(void);
 char *areafix_areasbbs(void);
@@ -204,7 +203,7 @@ int main(int argc, char **argv)
     int n_flag = FALSE;
     char *c_flag = NULL;
     char *a_flag = NULL, *u_flag = NULL;
-    int areafix = TRUE;
+    const char *areafix = "af";
     int w_flag = FALSE;
     Node node;
     int ret;
@@ -255,7 +254,7 @@ int main(int argc, char **argv)
             areafix_set_areasbbs(optarg);
             break;
         case 'F':
-            areafix = FALSE;
+            areafix = "ff";
             break;
 
     /***** Common options *****/

src/areafix/ftnafpkt.c
@@ -40,7 +40,6 @@
 /* Prototypes */
 
 /* areafix.c */
-void areafix_init(int);
 void areafix_auth_cmd(void);
 char *areafix_areasbbs(void);
 void areafix_set_areasbbs(char *name);
@@ -374,7 +373,7 @@ int main(int argc, char **argv)
     char *pkt_name;
     int w_flag = FALSE;
     char *cs_in = NULL, *cs_out = NULL, *p;
-    int areafix = TRUE;
+    const char *areafix = "af";
     char bbslock[MAXPATH];
 
     int option_index;
@@ -426,7 +425,7 @@ int main(int argc, char **argv)
             areafix_set_areasbbs(optarg);
             break;
         case 'F':
-            areafix = FALSE;
+            areafix = "ff";
             break;
         case 's':
             s_flag = TRUE;

src/areafix/ftnafutil.c
@@ -38,7 +38,6 @@
 /*
  * Prototypes
  */
-void areafix_init(int);
 void areafix_auth_cmd(void);
 char *areafix_areasbbs(void);
 void areafix_set_areasbbs(char *name);
@@ -555,7 +554,7 @@ int main(int argc, char **argv)
     pkt_outdir(O_flag ? O_flag : cf_p_outpkt(), NULL);
 
     /* Common init */
-    areafix_init(areafix);
+    areafix_init(areafix ? "af" : "ff");
 
     /* Read UPLINKS */
     uplinks_init();

src/common/passwd.c
@@ -118,7 +118,7 @@ void passwd_init(void)
 /*
  * Lookup password in list
  */
-Passwd *passwd_lookup(char *context, Node * node)
+Passwd *passwd_lookup(const char *context, Node * node)
 {
     Passwd *p;
 

src/include/prototypes.h
@@ -547,7 +547,7 @@ char *nf1(Node *);
 int wild_compare_node(Node *, Node *);
 /* passwd.c */
 void passwd_init(void);
-Passwd *passwd_lookup(char *, Node *);
+Passwd *passwd_lookup(const char *, Node *);
 void passwd_free(void);
 
 /* read.c */
@@ -725,5 +725,7 @@ MIMEInfo *get_mime(char *, char *, char *);
 MIMEInfo *get_mime_from_header(RFCHeader *);
 /* xstrnlen.c */
 size_t xstrnlen(const char *, size_t);
+
+void areafix_init(const char *);
 
 #endif

src/tick/ftntick.c
@@ -84,7 +84,6 @@ void usage(void);
 short check_pass(Tick *, short);
 int filefix_check_forbidden_area(char *);
 int cmd_new_int(Node *, char *, char *);
-void areafix_init(int);
 int areafix_auth_check(Node *, char *, char);
 char *areafix_name(void);
 
@@ -318,7 +317,7 @@ int process_tic(Tick * tic)
                 if (!check_pass(tic, TRUE))
                     return ERROR;
 
-            areafix_init(FALSE);
+            areafix_init(MY_CONTEXT);
             areafix_auth_check(&tic->from, tic->pw, TRUE);
 
             if (!authorized_new) {
@@ -530,7 +529,7 @@ int process_tic(Tick * tic)
 
             tl_init(&req);
 
-            areafix_init(FALSE);
+            areafix_init(MY_CONTEXT);
 
             if (!authorized_new)
                 areafix_auth_check(&from_node, tic->pw, TRUE);

src/toss/ftntoss.c
@@ -83,7 +83,6 @@ void usage(void);
 extern int authorized_new;
 static char *areas_bbs = NULL;
 
-void areafix_init(int);
 int areafix_auth_check(Node *, char *, char);
 int cmd_new_int(Node *, char *, char *);
 int areafix_check_forbidden_area(char *);
@@ -795,7 +794,7 @@ int do_echomail(Packet * pkt, Message * msg, MsgBody * body)
 
         AreaUplink *a;
 
-        areafix_init(TRUE);
+        areafix_init("af");
 
         areafix_auth_check(&msg->node_from, NULL, FALSE);
 